8ead6f6defd6de714bd9d090ef6df887da645d43
MengShell <1012509506@qq.com>
2020-09-17 17:03:06 +0800

    添加start.sh

--- src/main/java/com/jenkinstest/test/service/controller/IndexController.java
+++ src/main/java/com/jenkinstest/test/service/controller/IndexController.java
@@ -2,15 +2,16 @@ package com.jenkinstest.test.service.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: Meng
  * @Date: 2020/9/17 16:35
  */
-@Controller
+@RestController("index")
 public class IndexController {
 
-	@GetMapping("index")
+	@GetMapping
 	public String indexController(){
 
 		return "index.html";
